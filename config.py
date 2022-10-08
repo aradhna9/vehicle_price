cb74e5a2acc7732526cf7b12b5324a34b45ea47c
aradhna9 <110651708+aradhna9@users.noreply.github.com>
2022-10-08 22:59:28 +0530

    .

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+CSV_FILE_PATH = 'enter_path'
+
+PORT_NUMBER = 5007
+
+MODEL_FILE_PATH = r'E:\Vehicle_price\project_data\Linear_Model.pkl'
+
+JSON_FILE_PATH = r'E:\Vehicle_price\project_data\Label_Encoded_columns.json'
